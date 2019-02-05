70d36eb8b69a5c9213642593e04d233b53870d99
Kurt Heimbigner <10818122@uvu.edu>
2019-02-04 21:17:44 -0700

    Adding documents and JS to files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
 // JavaScript Document
 
-"use Strict";
+// "use Strict";
 
 document.querySelector('#topHeaderOne').textContent="Inspector Closeau";
 document.querySelector('#topHeaderTwo').textContent="Le Pink Panther";
+
+
+
+// Window Properties
+
+function windowSize(){
+var winHeight = window.innerHeight;
+var winWidth = window.innerWidth;
+var winUrl = window.location;
+
+
+var windowSize =  "The size of this window is " + winHeight + " pixels high by " + winWidth + " pixels wide.";
+windowSize += "<br> The window is offset roughly " + screenLeft + " pixels from the left edge and " + screenTop + " pixels from the top.";
+windowSize += "<br> The url for this window is " + winUrl;
+
+document.getElementById("windowParameters").innerHTML = windowSize;
+
+}
+
+
+windowSize();
+
+var docTitle = document.title;
+var lastUse = document.lastModified;
+
+var documentProps = " The title of this document is " + docTitle + ".";
+documentProps+= "<br>This page was last updated on " + lastUse + ". ";
+
